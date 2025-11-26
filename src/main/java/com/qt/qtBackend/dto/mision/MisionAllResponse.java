51dc6ddc29e83db061bf5d2ae3768cdf6859dfd3
Diego CH <158114096+diego-ch-21@users.noreply.github.com>
2025-11-26 00:08:01 -0500

    guardar

--- src/main/java/com/qt/qtBackend/dto/mision/MisionAllResponse.java
+++ src/main/java/com/qt/qtBackend/dto/mision/MisionAllResponse.java
@@ -2,7 +2,10 @@ package com.qt.qtBackend.dto.mision;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.qt.qtBackend.Enum.EstadoMisionEnum;
+import com.qt.qtBackend.dto.docenteInstitucion.DocenteIAllResponse;
+import com.qt.qtBackend.dto.docenteInstitucion.DocenteInstitucionAllResponse;
 import com.qt.qtBackend.dto.institucion.InstitucionShortResponse;
+import com.qt.qtBackend.model.DocenteInstitucion;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,6 +27,7 @@ public class MisionAllResponse {
     private ZonedDateTime fechaInicio;
     private ZonedDateTime fechaFin;
     private InstitucionShortResponse institucion;
+    private DocenteIAllResponse docenteInstitucion;
     private ZonedDateTime createdAt;
     private ZonedDateTime updatedAt;
     private Boolean enabled;
