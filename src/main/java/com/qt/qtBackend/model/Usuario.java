3cdd9d6076f0e4e27cdd72a8b20afcea0f2f9582
Diego CH <158114096+diego-ch-21@users.noreply.github.com>
2025-12-22 11:32:56 -0500

    cambio

--- src/main/java/com/qt/qtBackend/model/Usuario.java
+++ src/main/java/com/qt/qtBackend/model/Usuario.java
@@ -21,7 +21,7 @@ import java.util.List;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "usuario")
-public class Usuario implements UserDetails {
+public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
@@ -63,28 +63,5 @@ public class Usuario implements UserDetails {
     private Boolean enabled = true;
 
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(rol.getNombre()));
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-    @Override
-    public String getUsername() {
-        return email;
-    }
+
 }
